939be70fee40e98ff2607dcd1ceb25faa4734b7e
Alan W <awangc31s@gmail.com>
2025-05-21 16:03:00 -0400

    Fixed the header

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,6 +11,67 @@ function Paragraph(props: { children: React.ReactNode }) {
   );
 }
 
+function Faces() {
+  return (
+    <div className="bg-[#dedeae] rounded-2xl mt-10 p-8 shadow-md mx-auto">
+      <p className="text-lg font-semibold mb-6">
+        We, the Board of the Brown Outing Club, will abide by the following
+        values, and use them as guiding principles for the short and long-term
+        goals of the club (F.A.C.E.S.):
+      </p>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8 text-[#3f9135] font-semibold text-3xl">
+        <div>
+          <h2>Fun</h2>
+          <p className="text-black font-normal text-base mt-2">
+            afford the Brown community with safe and enjoyable recreation and
+            social interaction where participants can be themselves, take
+            healthy risks, and find a change of pace from the campus
+            environment;
+          </p>
+        </div>
+        <div>
+          <h2>Accessibility</h2>
+          <p className="text-black font-normal text-base mt-2">
+            acknowledge the myriad of barriers to outdoor recreation and
+            facilitate access for Brown community members of all identities,
+            backgrounds, experience levels, and financial circumstances;
+            actively reach individuals and groups who might not otherwise get
+            outside;
+          </p>
+        </div>
+        <div>
+          <h2>Community</h2>
+          <p className="text-black font-normal text-base mt-2">
+            foster social interaction and interpersonal connection by
+            encouraging collaboration and creating an inclusive, synergistic
+            relationship between leaders and participants;
+          </p>
+        </div>
+        <div>
+          <h2>Experience</h2>
+          <p className="text-black font-normal text-base mt-2">
+            craft spaces in which leaders and participants can have meaningful
+            interactions with themselves, their peers, and the surrounding
+            environment;
+          </p>
+        </div>
+        <div className="md:col-span-2 flex justify-center">
+          <div className="w-[50%]">
+            <h2>Service</h2>
+            <p className="text-black font-normal text-base mt-2">
+              serve the Brown community by providing opportunities to get off
+              campus and develop skills in a variety of outdoor pursuits;
+              instill respect for natural places, a commitment to Leave No Trace
+              ethics, and best practices for environmental stewardship.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function About() {
   return (
     <div className="h-full w-full px-20 py-10">
@@ -40,67 +101,13 @@ export default function About() {
             renewal, join our 3,000+ member mailing list of students, faculty,
             and staff. Want to support our mission? Donate to the BOC!
           </Paragraph>
+
         </div>
         <div>
           <img src={Image1.src} className="rounded-xl h-auto" />
         </div>
       </div>
-      <div className="bg-[#dedeae] rounded-2xl mt-10 p-8 shadow-md mx-auto">
-        <p className="text-lg font-semibold mb-6">
-          We, the Board of the Brown Outing Club, will abide by the following
-          values, and use them as guiding principles for the short and long-term
-          goals of the club (F.A.C.E.S.):
-        </p>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8 text-[#3f9135] font-semibold text-3xl">
-          <div>
-            <h2>Fun</h2>
-            <p className="text-black font-normal text-base mt-2">
-              afford the Brown community with safe and enjoyable recreation and
-              social interaction where participants can be themselves, take
-              healthy risks, and find a change of pace from the campus
-              environment;
-            </p>
-          </div>
-          <div>
-            <h2>Accessibility</h2>
-            <p className="text-black font-normal text-base mt-2">
-              acknowledge the myriad of barriers to outdoor recreation and
-              facilitate access for Brown community members of all identities,
-              backgrounds, experience levels, and financial circumstances;
-              actively reach individuals and groups who might not otherwise get
-              outside;
-            </p>
-          </div>
-          <div>
-            <h2>Community</h2>
-            <p className="text-black font-normal text-base mt-2">
-              foster social interaction and interpersonal connection by
-              encouraging collaboration and creating an inclusive, synergistic
-              relationship between leaders and participants;
-            </p>
-          </div>
-          <div>
-            <h2>Experience</h2>
-            <p className="text-black font-normal text-base mt-2">
-              craft spaces in which leaders and participants can have meaningful
-              interactions with themselves, their peers, and the surrounding
-              environment;
-            </p>
-          </div>
-          <div className="md:col-span-2 flex justify-center">
-            <div className="w-[50%]">
-              <h2>Service</h2>
-              <p className="text-black font-normal text-base mt-2">
-                serve the Brown community by providing opportunities to get off
-                campus and develop skills in a variety of outdoor pursuits;
-                instill respect for natural places, a commitment to Leave No
-                Trace ethics, and best practices for environmental stewardship.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Faces />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,188 @@
 "use client";
+import React, { useEffect, useRef, useState } from "react";
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+
+import login from "@/scripts/login";
 import bear_vector from "@/assets/images/logo.svg";
-import SignIn from "@/components/SignIn";
 import { usePathname } from "next/navigation";
 
-function NavButton(props: { children: React.ReactNode; func: () => void }) {
+const handleLoginSuccess = async (response: { code: string }) => {
+  try {
+    const { data } = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_BASE}/auth`,
+      {
+        code: response.code,
+      },
+    );
+    localStorage.setItem("access_token", data.access_token);
+  } catch (error) {
+    console.error("Error during authentication", error);
+  }
+};
+
+const handleLoginFailure = (error: unknown) => {
+  console.error("Login failed", error);
+};
+
+const menuItems = [
+  {
+    label: "ABOUT",
+    url: "/about",
+    dropdown: [
+      { label: "About Us", url: "/about" },
+      { label: "Land Tribute", url: "/about/land-tribute" },
+    ],
+  },
+  {
+    label: "TRIPS",
+    url: "/trips",
+    dropdown: [],
+  },
+  {
+    label: "GEAR",
+    url: "/gear-room",
+    dropdown: [
+      { label: "Gear Room", url: "/gear-room" },
+      { label: "Policies", url: "/gear-room/policies" },
+    ],
+  },
+  {
+    label: "GET INVOLVED",
+    url: "/get-involved",
+    dropdown: [],
+  },
+];
+
+function NavButton(props: { item: any }) {
   const pathname = usePathname();
+  const isDropdown = props.item.dropdown.length > 0;
+
   return (
-    <button
-      onClick={() => {
-        window.location.href = props.url;
-      }}
-      className={`py-2 px-4 rounded-lg cursor-pointer font-montserrat font-bold text-[18px]
-      ${props.url == pathname ? "bg-boc_darkgreen text-white" : "bg-transparent text-boc_darkbrown"}`}
-    >
-      {props.children}
-    </button>
+    <div className="relative group min-w-[150px]">
+      <button
+        className={`w-full h-full py-2 px-4 rounded-lg 
+        ${isDropdown ? "group-hover:rounded-b-none" : ""}
+          cursor-pointer font-montserrat font-bold
+      ${pathname == props.item.url ? "bg-boc_green text-white" : "bg-transparent text-boc_darkbrown"}`}
+        onClick={() => {
+          window.location.href = props.item.url;
+        }}
+      >
+        {props.item.label}
+      </button>
+
+      {isDropdown ? (
+        <ul
+          className="absolute top-full w-full bg-transparent text-[12pt]
+      text-boc_darkbrown rounded-b-md opacity-0 invisible border-[2px] border-boc_green
+      group-hover:visible group-hover:opacity-100 z-10"
+        >
+          {props.item.dropdown.map((option, i) => (
+            <li
+              key={i}
+              onClick={() => {
+                window.location.href = option.url;
+              }}
+              className={`px-2 py-2 cursor-pointer
+              ${i > 0 ? "border-boc_darkgreen border-t-[2px]" : ""}`}
+            >
+              {option.label}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
   );
 }
 
 function NavBar() {
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const updateLogin = async () => {
+    try {
+      let token = localStorage.getItem("access_token");
+      if (!token) {
+        setUser("");
+        return;
+      }
+
+      const { data: userData } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE}/user/profile`,
+        {
+          headers: {
+            token: token,
+          },
+        },
+      );
+      setUser(`${userData.firstName} ${userData.lastName}`);
+      setLoading(false);
+    } catch (error) {
+      setUser("");
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    updateLogin();
+  }, []);
+
+  const login = useGoogleLogin({
+    onSuccess: handleLoginSuccess,
+    onError: handleLoginFailure,
+    flow: "auth-code",
+  });
+
+  const userItem = {
+    label: user,
+    url: "/user",
+    dropdown: [
+      { label: "Profile", url: "/user" },
+      { label: "Logout", url: "/logout" },
+    ],
+  };
+
   return (
-    <div className="w-screen flex justify-between px-8 py-4 z-10">
+    <nav className="px-8 py-4 flex justify-between">
       <a href="/">
         <img
           src={bear_vector.src}
           alt="Bear Vector"
-          className="cursor-pointer"
+          className="cursor-pointer absolute"
         />
       </a>
-      <div className="flex space-x-5 py-4">
-        <NavButton url="/">HOME</NavButton>
-        <NavButton url="/about">ABOUT</NavButton>
-        <NavButton url="/trips">TRIPS</NavButton>
-        <NavButton url="/gear-room">GEARS</NavButton>
-        <NavButton url="/get-involved">GET INVOLVED</NavButton>
-        <SignIn />
-      </div>
-    </div>
+      {loading ? (
+        <div className="min-w-[200px] min-h-[80px]"></div>
+      ) : (
+        <div className="flex space-x-10 py-4 min-h-[80px]">
+          {menuItems.map((item, index) => (
+            <NavButton key={index} item={item} />
+          ))}
+
+          <div className="min-w-[200px]">
+            {user ? (
+              <NavButton item={userItem} />
+            ) : (
+              <button
+                onClick={() => {
+                  login();
+                }}
+                className="font-bold font-montserrat text-boc_darkbrown"
+              >
+                LOGIN
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </nav>
   );
+}
+
+async function logout() {
+  localStorage.removeItem("access_token");
+  window.location.href = "/";
 }
 
 export default NavBar; // Make sure to export the component
