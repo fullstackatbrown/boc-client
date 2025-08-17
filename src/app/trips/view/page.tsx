45f51abf00b65dd061a6ae693342ef044866e886
Alan W <awangc31s@gmail.com>
2025-08-16 20:16:33 -0400

    Fixed suspense wrapper

--- src/app/trips/view/page.tsx
+++ src/app/trips/view/page.tsx
@@ -4,46 +4,68 @@ import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 
 import { useSession } from "next-auth/react";
-import { AuthStat, makeRequesters }from "@/scripts/requests";
+import { AuthStat, makeRequesters } from "@/scripts/requests";
 import { TripWithSignup } from "@/models/models";
+
+import { Suspense } from "react";
 
 import TripPageContents from "./TripPageContents";
 
-export default function TripPage() {
+function TripPage() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
 
-  const [trip, setTrip] = useState<TripWithSignup|null>(null);
+  const [trip, setTrip] = useState<TripWithSignup | null>(null);
 
   const reqs = makeRequesters();
   const { backendGet, sessionStatus } = reqs;
 
   useEffect(() => {
-    sessionStatus().then((stat: AuthStat) => {
-      let noAuth = (stat == AuthStat.Unauth);
-      backendGet(`/trip/${id}`, noAuth)
-        .then((res): void => { setTrip(res.data); })
-        .catch((err): void => {
-          console.log("ERRORED!!")
-          switch (err.status) {
-            case (401):
-              alert("The requested trip is still in staging. Log in with a trip leader's account or wait for the trip to be made public!")
-              break;
-            case (404):
-              alert("The requested trip page does not exist.")
-              break;
-            default:
-              alert(`ERROR. You shouldn't be seeing this! Contact the website's admin and send them a picture of this message! ${err}`)
-              break;
-          }
-          window.location.href = '/trips'
-        })
-    }).catch((err) => { alert(err) })
+    sessionStatus()
+      .then((stat: AuthStat) => {
+        let noAuth = stat == AuthStat.Unauth;
+        backendGet(`/trip/${id}`, noAuth)
+          .then((res): void => {
+            setTrip(res.data);
+          })
+          .catch((err): void => {
+            console.log("ERRORED!!");
+            switch (err.status) {
+              case 401:
+                alert(
+                  "The requested trip is still in staging. Log in with a trip leader's account or wait for the trip to be made public!",
+                );
+                break;
+              case 404:
+                alert("The requested trip page does not exist.");
+                break;
+              default:
+                alert(
+                  `ERROR. You shouldn't be seeing this! Contact the website's admin and send them a picture of this message! ${err}`,
+                );
+                break;
+            }
+            window.location.href = "/trips";
+          });
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }, []);
 
   if (!trip) {
     return <div className="px-20 flex justify-center">Loading...</div>;
   } else {
-    return <TripPageContents trip={trip!} reqs={reqs}/>
+    return <TripPageContents trip={trip} reqs={reqs} />;
   }
+}
+
+export default function TripsWrapper() {
+  return (
+    <Suspense
+      fallback={<div className="px-20 flex justify-center">Loading...</div>}
+    >
+      <TripPage />
+    </Suspense>
+  );
 }
